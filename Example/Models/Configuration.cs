746c5ac3d5c684ca962f8b42b5cb49671d203e5b
Tooraj Helmi <thelmi@usc.edu>
2020-12-08 17:22:23 -0800

    Removed extra projects

--- Example/Models/Configuration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Apsy.Elemental.Example.Web.Models
-{
-    public class Configuration
-    {
-        public int ConfigurationId { get; set; }
-        public string Key { get; set; }
-        public string Value { get; set; }
-    }
-}
